6fc8b0cff9f8174d6e0578ace467e750ddb6e3e8
Zeldina Chalco <dazelcolab2016@gmail.com>
2016-12-20 10:38:02 -0500

    run ejercicio-3

---
+++ main.js
@@ -0,0 +1,13 @@
+var horas, pago, sueldoSemanal;
+horas = prompt("Por favor ingresa numero de horas trabajadas");
+pago = prompt("Por favor ingresa el monto de pago en $");
+sueldoSemanal = parseFloat(horas) * parseFloat(pago);
+alert (sueldoSemanal);
+
+
+
+
+
+
+
+
